7c242682ae79031ec25a2d0ff15cf424dba9992f
kaytee07 <kofi.taylor07@gmail.com>
2024-03-30 13:03:21 +0000

    t.map error

--- ai-prompt/components/Feed.jsx
+++ ai-prompt/components/Feed.jsx
@@ -6,7 +6,6 @@ import PromptCard from "./PromptCard";
 const PromptCardList = ({ data, handleTagClick}) => {
   return (
     <div className="mt-16 prompt_layout">
-      {alert(data)}
       {data.map((post) => {
         return (
           <PromptCard
