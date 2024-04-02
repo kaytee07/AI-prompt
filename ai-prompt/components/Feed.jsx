24af77e164f62fceaa21c7a310c1960e4c257155
kaytee07 <kofi.taylor07@gmail.com>
2024-04-02 08:13:47 +0000

    feed update on post or delete

--- ai-prompt/components/Feed.jsx
+++ ai-prompt/components/Feed.jsx
@@ -47,8 +47,8 @@ const Feed = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch("/api/prompt", {
-        cache: "no-store"
+      const response = await fetch("/api/prompt",{
+        cache:"no-cache"
       });
       const data = await response.json();
       console.log(data)
